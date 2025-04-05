a32995b6a6be70c542e9903b65ff97e70c051249
Ankit <as6553@srmist.edu.in>
2025-04-06 00:47:13 +0530

    Vendor Management Module - .Net

---
+++ eventzen-backend/vendorManagement_dotNet/VendorManagementAPI/Models/EventVendor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VendorManagementAPI.Models
+{
+	public class EventVendor
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int EventId { get; set; }
+
+		[Required]
+		public int VendorId { get; set; }
+
+		[ForeignKey("VendorId")]
+		public Vendor? Vendor { get; set; }
+	}
+}
